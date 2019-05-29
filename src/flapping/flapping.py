957fdbd1b1de4c16cf22648960c2cd30b7dfb830
SirGnip <SirGnip@users.noreply.github.com>
2019-05-29 16:59:21 -0500

    Slightly better flying and collision

--- src/flapping/flapping.py
+++ src/flapping/flapping.py
@@ -1,16 +1,15 @@
 import arcade
 
-GRAVITY = 0.2
 
 class Player(arcade.Sprite):
     MOVEMENT_SPEED = 3  # pixels per frame
-    JUMP_SPEED = 6
+    JUMP_SPEED = 3
 
     def __init__(self, img):
         super().__init__(img)
 
     def on_up(self):
-        self.change_y = self.JUMP_SPEED
+        self.change_y += self.JUMP_SPEED
 
     def on_down(self):
         self.change_y = -self.MOVEMENT_SPEED
@@ -21,20 +20,14 @@ class Player(arcade.Sprite):
     def on_right(self):
         self.change_x = self.MOVEMENT_SPEED
 
-    def on_key_x_release(self):
-        self.change_x = 0
-
-    def on_key_y_release(self):
-        self.change_y = 0
-
 
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
         self.map = arcade.read_tiled_map('map.tmx')
-        self.wall_list = arcade.generate_sprites(self.map, 'walls', 1.0)
-        for s in self.wall_list.sprite_list:
+        self.walls = arcade.generate_sprites(self.map, 'walls', 1.0)
+        for s in self.walls.sprite_list:
             s.center_x += 32
 
         self.player1 = Player("img/duck.png")
@@ -48,9 +41,6 @@ class MyGame(arcade.Window):
         self.player_list.append(self.player1)
         self.player_list.append(self.player2)
 
-        self.physics_engine1 = arcade.PhysicsEnginePlatformer(self.player1, self.wall_list, GRAVITY)
-        self.physics_engine2 = arcade.PhysicsEnginePlatformer(self.player2, self.wall_list, GRAVITY)
-
         self.controller_press = {
             arcade.key.UP:    self.player1.on_up,
             arcade.key.DOWN:  self.player1.on_down,
@@ -62,36 +52,37 @@ class MyGame(arcade.Window):
             arcade.key.D: self.player2.on_right,
             arcade.key.ESCAPE: self.close,
         }
-        self.controller_release = {
-            arcade.key.UP:    self.player1.on_key_y_release,
-            arcade.key.DOWN:  self.player1.on_key_y_release,
-            arcade.key.LEFT:  self.player1.on_key_x_release,
-            arcade.key.RIGHT: self.player1.on_key_x_release,
-            arcade.key.W: self.player2.on_key_y_release,
-            arcade.key.S: self.player2.on_key_y_release,
-            arcade.key.A: self.player2.on_key_x_release,
-            arcade.key.D: self.player2.on_key_x_release,
-        }
 
     def setup(self):
         pass
 
     def on_draw(self):
         arcade.start_render()
-        self.wall_list.draw()
+        self.walls.draw()
         self.player_list.draw()
 
     def on_key_press(self, key, modifiers):
         if key in self.controller_press:
             self.controller_press[key]()
 
-    def on_key_release(self, key, modifiers):
-        if key in self.controller_release:
-            self.controller_release[key]()
-
     def update(self, delta_time):
-        self.physics_engine1.update()
-        self.physics_engine2.update()
+        self.player_list.update()
+        for p in self.player_list:
+            p.change_y -= 0.2 # gravity
+        self.physics(self.player1, self.walls)
+        self.physics(self.player2, self.walls)
+
+    def physics(self, player, walls):
+        hit_list = arcade.geometry.check_for_collision_with_list(player, walls)
+        # If we hit a wall, move so the edges are at the same point
+        if len(hit_list) > 0:
+            for item in hit_list:
+                if player.center_y > item.center_y:
+                    player.bottom = item.top
+                    player.change_y = 0
+                elif player.center_y < item.center_y:
+                    player.top = item.bottom
+                    player.change_y = 0
 
 
 def main():
